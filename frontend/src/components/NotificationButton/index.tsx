7a11c4e8f6fbdfadebe6b8ccf0321cd22552db18
F. Rafael C. de Castro <rafaeldecastro@outlook.com>
2022-07-16 15:03:03 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -10,8 +11,8 @@ type Props = {
 
 function handleClick(id: number) {
     axios.get(`${BASE_URL}/sales/${id}/notification`)
-        .then(response => {
-            console.log("SUCESSO");
+        .then(() => {
+            toast.success('SMS enviado com sucesso');
         })
 }
 
